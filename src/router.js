e6ee01977de79832d70dfffe342135715036763e
Gustavo Maysonnave Franck <gmaysonnavefranck@gmail.com>
2022-11-02 21:45:34 -0300

    adding base components

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from '@/store/store'
 import EventCreate from './views/EventCreate.vue'
 import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
+import NotFound from './views/NotFound.vue'
+import NetworkIssue from './views/NetworkIssue.vue'
 
 Vue.use(Router)
 
@@ -24,7 +27,40 @@ export default new Router({
       path: '/event/:id',
       name: 'event-show',
       component: EventShow,
+      props: true,
+      beforeEnter(routeTo, routeFrom, next) {
+        store
+          .dispatch('event/fetchEvent', routeTo.params.id)
+          .then(event => {
+            routeTo.params.event = event
+            next()
+          })
+          .catch(error => {
+            if (error.response && error.response.status == 404) {
+              next({
+                name: '404',
+                params: { resource: 'event' }
+              })
+            } else {
+              next({ name: 'network-issue' })
+            }
+          })
+      }
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: NotFound,
       props: true
+    },
+    {
+      path: '/network-issue',
+      name: 'network-issue',
+      component: NetworkIssue
+    },
+    {
+      path: '*',
+      redirect: { name: '404', params: { resource: 'page' } }
     }
   ]
 })
